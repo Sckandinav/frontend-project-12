dcec8f93a333756a7d0f24ba6e652bcbdadb5ef9
Vasili <ballov_v@mail.ru>
2023-11-16 12:10:43 +0300

    rollbar/react installed and added to project

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -8,14 +8,17 @@ import ChatProvider from './contexts/ChatProvider';
 import AuthProvider from './contexts/AuthProvider';
 import App from './components/App';
 import resources from './locales/index';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 
 const DEFAULT_LANGUAGE = 'ru';
 
-const socket = io();
-
 const init = async () => {
-  const russianDictionary = leoProfanity.getDictionary('ru');
-  leoProfanity.add(russianDictionary);
+  const rollbarConfig = {
+    accessToken: 'POST_CLIENT_ITEM_ACCESS_TOKEN',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    environment: 'production',
+  };
 
   const i18nextInstance = i18next.createInstance();
 
@@ -29,14 +32,23 @@ const init = async () => {
     },
   });
 
+  const socket = io();
+
+  const russianDictionary = leoProfanity.getDictionary('ru');
+  leoProfanity.add(russianDictionary);
+
   return (
-    <I18nextProvider i18n={i18nextInstance}>
-      <AuthProvider>
-        <ChatProvider socket={socket}>
-          <App />
-        </ChatProvider>
-      </AuthProvider>
-    </I18nextProvider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <I18nextProvider i18n={i18nextInstance}>
+          <AuthProvider>
+            <ChatProvider socket={socket}>
+              <App />
+            </ChatProvider>
+          </AuthProvider>
+        </I18nextProvider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
